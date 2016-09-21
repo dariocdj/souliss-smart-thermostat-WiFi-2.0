8735f511bee5da9b3892b08deb1ed4389cccec49
mcbittech <master.flavio.piccolo@gmail.com>
2016-09-21 08:16:04 +0200

    first test with esp8266(wemos)+serial level converter and DHT22

--- studio/SST2_v/SST2_v.ino
+++ studio/SST2_v/SST2_v.ino
@@ -1,5 +1,18 @@
 
 
+#include "DHT.h"
+
+#define DHTPIN 5     // Pin DHT22
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
+DHT dht(DHTPIN, DHTTYPE);
+
+
+int brightmin=1;
+int brightmax=100;
+int brightness = brightmin;    // how bright the LED is
+int fadeAmount = brightmin;    // how many points to fade the LED by
+
+
 int i=0;
 int temperatura=-1;
 int temperaturadec=0;
@@ -10,36 +23,62 @@ int umidita=0;
 void setup() {
   // initialize serial:
   Serial.begin(9600);
-  Serial1.begin(9600);
-  
-  Serial1.print("page 1");
-  Serial1.write(0xff);
-  Serial1.write(0xff);
-  Serial1.write(0xff);
-  delay(5000);
+Serial.print("dim=");Serial.print(1);
+  Serial.write(0xff);
+  Serial.write(0xff);
+  Serial.write(0xff);
+  delay(1000);
+  Serial.print("page 1");
+  Serial.write(0xff);
+  Serial.write(0xff);
+  Serial.write(0xff);
+Serial.print("dim=");Serial.print(100);
+  Serial.write(0xff);
+  Serial.write(0xff);
+  Serial.write(0xff);  
+  dht.begin();
 }
 
 void loop() {
-
-Serial1.print("n0.val=");Serial1.print(temperatura);
-  Serial1.write(0xff);
-  Serial1.write(0xff);
-  Serial1.write(0xff);
-Serial1.print("n1.val=");Serial1.print(temperaturadec);
-  Serial1.write(0xff);
-  Serial1.write(0xff);
-  Serial1.write(0xff);
-Serial1.print("n2.val=");Serial1.print(umidita);
-  Serial1.write(0xff);
-  Serial1.write(0xff);
-  Serial1.write(0xff);    
+/*
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);  
+*/
 
 
-temperatura = random(-1,45);
+Serial.print("n0.val=");Serial.print(temperatura);
+  Serial.write(0xff);
+  Serial.write(0xff);
+  Serial.write(0xff);
+Serial.print("n1.val=");Serial.print(temperaturadec);
+  Serial.write(0xff);
+  Serial.write(0xff);
+  Serial.write(0xff);
+Serial.print("n2.val=");Serial.print(umidita);
+  Serial.write(0xff);
+  Serial.write(0xff);
+  Serial.write(0xff);    
+Serial.print("dim=");Serial.print(brightness);
+  Serial.write(0xff);
+  Serial.write(0xff);
+  Serial.write(0xff); 
+
+
+temperatura = dht.readTemperature();
 temperaturadec = random(0,9);
-umidita = random(1,99);
+umidita = dht.readHumidity();
+delay(100);
 
-delay(50);
+  brightness = brightness + fadeAmount;
+  
+  if (brightness == brightmin || brightness == brightmax) {
+    fadeAmount = -fadeAmount ;
+  }
 
 }
 
